2af0965d181146b86adaec12fc17516a306d7063
almela09 <145797671+almela09@users.noreply.github.com>
2024-04-08 11:29:54 +0200

    first commit, react project basic structure

---
+++ front-redes/src/pages/Login/Login.jsx
@@ -0,0 +1,6 @@
+import "./Login.css"
+export const Login = () => {
+    return <div className= " login-design">
+      hola login
+      </div>;
+  };
---
+++ front-redes/src/pages/Register/Register.jsx
@@ -0,0 +1,6 @@
+import "./Register.css"
+export const Register = () => {
+    return <div className= " register-design">
+      hola register
+      </div>;
+  };
