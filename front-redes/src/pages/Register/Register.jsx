eeaba13536c63cb48174f7717b2c47c3ac5db96d
almela09 <145797671+almela09@users.noreply.github.com>
2024-04-08 15:59:01 +0200

    feature: register-design more or less

--- front-redes/src/pages/Register/Register.jsx
+++ front-redes/src/pages/Register/Register.jsx
@@ -1,6 +1,31 @@
 import "./Register.css"
+import { FaUser, FaLock } from "react-icons/fa";
 export const Register = () => {
-    return <div className= " register-design">
-      hola register
-      </div>;
+    return (
+      <div className= "register-home">
+      <div className="wrapper-register">
+        <form action="">
+          <h1>Register</h1>
+          <div className="input-box">
+            <input type="text" placeholder='Email' required />
+            <FaUser className="icon-email" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder='Password' required />
+            <FaLock className="icon-lock" />
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" /> Remember me
+            </label>
+            <a href="#">Forgot password?</a>
+          </div>
+  
+          <button type="submit">Register</button>
+         
+        </form>
+      </div>
+      </div>
+    );
+    
   };
