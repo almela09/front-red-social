20b2a62f4340bcfaff0f6889023e7f4b466982ad
almela09 <145797671+almela09@users.noreply.github.com>
2024-04-11 11:23:52 +0200

    changed font-family

--- front-redes/src/common/PostCard/PostCard.jsx
+++ front-redes/src/common/PostCard/PostCard.jsx
@@ -1,18 +1,23 @@
 import "./PostCard.css"
-import { Card, CardContent, Typography } from '@mui/material';
+// import { Card, CardContent, Typography } from '@mui/material';
 
 const PostCard = ({ post }) => {
    
   return (
     <div className="box-design">
-    <Card sx={{ maxWidth: 345, marginBottom: 2 }}>
+        <div className="cardcontent-design">
+            <h5>{post.title}</h5>
+            <p>{post.text}</p>
+            <p>{post.author}</p>
+        </div>
+    {/* <Card className="box-design" sx={{ maxWidth: 345, marginBottom: 2 }}>
       <CardContent>
         <Typography variant="h5">{post.title}</Typography>
         <Typography paragraph>{post.text}</Typography>
         <Typography color="text.secondary">Author ID: {post.author}</Typography>
         <Typography color="text.secondary">Likes: {post.like.length}</Typography>
       </CardContent>
-    </Card>
+    </Card> */}
     </div>
   );
 };
