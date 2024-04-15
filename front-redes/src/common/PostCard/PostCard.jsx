2c99ee2d1122dfd13c86c7e6e40d41bb12d9aaeb
almela09 <145797671+almela09@users.noreply.github.com>
2024-04-15 20:10:37 +0200

    updated postCard

--- front-redes/src/common/PostCard/PostCard.jsx
+++ front-redes/src/common/PostCard/PostCard.jsx
@@ -1,40 +1,22 @@
 import "./PostCard.css"
 // import { Card, CardContent, Typography } from '@mui/material';
 import {useNavigate} from "react-router-dom";
-const PostCard = ({  post }) => {
-  const navigate = useNavigate()
+const PostCard = ({ post }) => {
+  const navigate = useNavigate();
+  
   const handleClick = () => {
-    navigate(`/post/${post._id}`)
+    navigate(`/post/${post._id}`);
+  }
+
+  return (
+    <div className="box-design" onClick={handleClick}>
+      <div className="cardcontent-design">
+        <h5>{post.title}</h5>
+        <p>{post.text}</p>
+        <p>{post.author}</p>
+      </div>
+    </div>
+  );
 }
-return (
-  <div className="box-design" onClick={handleClick}>
-  <div className="cardcontent-design">
-    <h5>{post.title}</h5>
-    <p>{post.text}</p>
-    <p>{post.author}</p>
-    <button onClick={(e) => {
-      e.stopPropagation(); // Esto evita que el evento del botón también active el evento del div padre
-      navigate(`/post/${post._id}`);
-    }} className="enter-post-button">
-      Enter Post
-    </button>
-  </div>
-</div>
 
-  // <div className="box-design" onClick={handleClick}>
-  //   <div className="cardcontent-design">
-  //     <h5>{post.title}</h5>
-  //     <p>{post.text}</p>
-  //     <p>{post.author}</p>
-  //     <button onClick={(e) => {
-  //       e.stopPropagation(); // Esto evita que el evento del botón también active el evento del div padre
-  //       navigate(`/post/${post._id}`);
-  //     }} className="enter-post-button">
-  //       Enter Post
-  //     </button>
-  //   </div>
-  // </div>
-);
-
-};
 export default PostCard;
